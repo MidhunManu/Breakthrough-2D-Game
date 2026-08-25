a5b69c0ecef748a75057c4eea35cd5345982a697
MidhunManu <midhunmanu384@gmail.com>
2026-08-25 16:25:37 +0530

    refactor: state struct feat: right and left faced movement

--- main.cpp
+++ main.cpp
@@ -59,10 +59,15 @@ int main(int argc, char *argv[])
 {
     SDL_State state{};
     initialize(state);
-    SDL_Event event;
+    const float sprite_size = 32;
     SDL_Texture *idleTexture =
         IMG_LoadTexture(state.renderer, "assets/idle.png");
     SDL_SetTextureScaleMode(idleTexture, SDL_SCALEMODE_NEAREST);
+
+    const bool* keys = SDL_GetKeyboardState(nullptr);
+    float playerX = 150;
+    float floor = state.logH;
+
     if (!idleTexture)
     {
         SDL_Log("IMG_LoadTexture failed: %s", SDL_GetError());
@@ -71,9 +76,14 @@ int main(int argc, char *argv[])
     }
 
     bool running = true;
-
+    uint64_t prev_time = SDL_GetTicks();
+    bool flip_horizontal = false;
     while (running)
     {
+        uint64_t now_time = SDL_GetTicks();
+        float delta_time = (now_time - prev_time) / 1000.0f;
+        SDL_Event event {0};
+
         while (SDL_PollEvent(&event))
         {
             switch (event.type)
@@ -92,16 +102,32 @@ int main(int argc, char *argv[])
             }
         }
 
+        float move_amount = 0;
+        if (keys[SDL_SCANCODE_A])
+        {
+            move_amount -= 75;
+            flip_horizontal = true;
+        }
+        if (keys[SDL_SCANCODE_D])
+        {
+            move_amount += 75;
+            flip_horizontal = false;
+        }
+
+        playerX += move_amount * delta_time;
+
         SDL_SetRenderDrawColor(state.renderer, 20, 10, 30, 255);
         SDL_RenderClear(state.renderer);
 
-        SDL_FRect src{0, 0, 32, 32};
+        SDL_FRect src{0, 0, sprite_size, sprite_size};
 
-        SDL_FRect dest{0, 0, 32, 32};
+        SDL_FRect dest{playerX, floor - sprite_size, sprite_size, sprite_size};
 
-        SDL_RenderTexture(state.renderer, idleTexture, &src, &dest);
+        SDL_RenderTextureRotated(state.renderer, idleTexture, &src, &dest, 0, nullptr, flip_horizontal ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE);
+
 
         SDL_RenderPresent(state.renderer);
+        prev_time = now_time;
     }
 
     cleanup_texture(idleTexture);
